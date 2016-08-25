42d7f206c00bb4126b08ceb4ac053978b433d542
Joseph Colicchio <jcolicchio@novacoast.com>
2016-08-24 22:33:22 -0700

    Test post; please ignore

--- test_commit.py
+++ test_commit.py
@@ -4,17 +4,41 @@ from subprocess import call
 from subprocess import check_output
 import threading
 
+# get the most recent commit
+updateMessage = ""
+recentCommit = ""
+
+def mostRecentCommitHash():
+	return check_output(["git", "log", "-1", "--oneline"])[:7]
+
+def sendMessage(msg):
+	global recentCommit
+	check_output(["git", "commit", "-m", msg, "--allow-empty"])
+	recentCommit = mostRecentCommitHash()
+	check_output(["git", "push"])
+
 def pollForUpdates():
-	result = check_output(["git", "pull"])
-	while(result.find("Already up-to-date.") != -1):
+	global updateMessage
+	global recentCommit
+	recentCommit = check_output(["git", "log", "--oneline"]).split('\n')[0][:7]
+	while True:
 		result = check_output(["git", "pull"])
-	print("GOT AN UPDATE")
+		while(result.find("Already up-to-date.") != -1):
+			if len(updateMessage) > 0:
+				sendMessage(updateMessage)
+				updateMessage = ""
+			result = check_output(["git", "pull"])
+		allLogs = check_output(["git", "log", "--oneline"]).split('\n')
+		print(allLogs)
+		i = 0
+		while i < len(allLogs) and allLogs[i][:7] != recentCommit:
+			print(allLogs[i][8:])
+			i += 1
+		recentCommit = allLogs[0][:7]
 
 t = threading.Thread(target=pollForUpdates, args=())
 t.start()
 
 while True:
-	a = raw_input()
-	call(["git", "commit", "-m", "Test commit", "--allow-empty"])
-	call(["git", "push"])
+	updateMessage = raw_input()
 
